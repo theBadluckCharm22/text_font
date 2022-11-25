1ccd4e3f118ecb6e23c9b5170600d48806915925
theBadluckCharm22 <118781176+theBadluckCharm22@users.noreply.github.com>
2022-11-24 20:50:10 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+handX=0;
+handY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
+
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized!');
+}
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    handX = results[0].pose.hand.x;
+    handY = results[0].pose.hand.y;
+    console.log("handX = " + noseX +" handY = " + noseY);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
+}
+
+function draw() {
+background('#F476BC');
+
+  document.getElementById("text_side").innerHTML = "Width And Height of the text will be = " + difference +"px";
+  fill('#76F4DC');
+  stroke('#47086C');
+  text(handX, handY, difference);
+}
